b82fb7265ea3a3d84077e621c79b8efe6d8d8946
Tim0082 <timsciortino@icloud.com>
2025-07-11 00:09:39 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const burger = document.getElementById('burger');
+const menu = document.getElementById('menu');
+
+burger.addEventListener('click', () => {
+  if (menu.style.display === 'flex') {
+    menu.style.display = 'none';
+  } else {
+    menu.style.display = 'flex';
+  }
+});
